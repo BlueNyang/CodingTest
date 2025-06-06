b924bcb8e34baae0ca7d0a281e874c011b8252a5
BlueNyang <xpsj20@gmail.com>
2025-06-06 18:50:44 +0900

    [level 0] Title: 모스부호 (1), Time: 15.30 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(letter: String): String {
+        val morse: Map<String, String> = mapOf(".-" to "a", "-..." to "b", "-.-." to "c", "-.." to "d", "." to "e", "..-." to "f", "--." to "g", "...." to "h", ".." to "i", ".---" to "j", "-.-" to "k", ".-.." to "l", "--" to "m", "-." to "n", "---" to "o", ".--." to "p", "--.-" to "q", ".-." to "r", "..." to "s", "-" to "t", "..-" to "u", "...-" to "v", ".--" to "w", "-..-" to "x", "-.--" to "y", "--.." to "z")
+        
+        var answer: String = ""
+        
+        val words = letter.split(' ');
+        words.forEach { word ->
+            answer += morse[word]
+        }
+        
+        return answer
+    }
+}
