0b74191b1ab7af3e78872e5bf0d76cb9f8540712
BlueNyang <xpsj20@gmail.com>
2025-06-18 22:45:30 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
+++ C++/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -3,21 +3,26 @@
 
 using namespace std;
 
+// switch 문 사용
+char get_to_win(char a) {
+    switch(a) {
+        case '0':
+            return '5';
+        case '2':
+            return '0';
+        case '5':
+            return '2';
+        default:
+            return '-1';
+    }
+    return '-1';
+}
+
 string solution(string rsp) {
     string answer = rsp;
     
     for (auto& a: answer) {
-        switch(a) {
-            case '0':
-                a = '5';
-                break;
-            case '2':
-                a = '0';
-                break;
-            default:
-                a = '2';
-                break;
-        }
+        a = get_to_win(a);
     }
     
     return answer;
