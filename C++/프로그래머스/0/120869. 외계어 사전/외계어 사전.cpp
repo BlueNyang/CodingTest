a49cda0f980e8686dc503a4a8a96c1da0d786846
BlueNyang <xpsj20@gmail.com>
2025-06-21 22:37:22 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- C++/프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
+++ C++/프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
@@ -3,26 +3,38 @@
 
 using namespace std;
 
+// algorithm의 count와 비슷함.
 int count_str(string str, char target) {
     int count = 0;
+    // 순회하면서 target의 갯수를 세서 반환
     for (const auto& c: str) {
         if (c == target) count++;
     }
+    
     return count;
 }
 
 int solution(vector<string> spell, vector<string> dic) {
-    bool flag = false;
+    // 찾았는지 확인하는 FLAG
+    bool __FOUND_FLAG__ = false;
+    
+    // 순회
     for(const auto& str: dic) {
+        // 스펠 순회
         for (const auto& s: spell) {
+            // 하나만 들어있지 않으면 FLAG 회수 후 break;
             if (count_str(str, s[0]) != 1) {
-                flag = false;
+                __FOUND_FLAG__ = false;
                 break;
             }
-            flag = true;
+            // break가 되지 않음 == 1만 있음
+            // str에 spell 요소가 하나씩 만 있으면 FLAG는 유지됨
+            __FOUND_FLAG__ = true;
         }
-        if (flag) return 1;
+        // FLAG가 있으면 1반환
+        if (__FOUND_FLAG__) return 1;
     }
     
+    // 없으면 2반환
     return 2;
 }
