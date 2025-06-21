a64f4dc1f65538c9baa44b54860dcdc8cfe09f08
BlueNyang <xpsj20@gmail.com>
2025-06-21 21:24:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- C++/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
+++ C++/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -8,16 +8,24 @@ int solution(string my_string) {
     int temp = 0;
     
     for(const auto c: my_string) {
+        // 글자의 ASCii 코드 값이 0 ~ 9 이면
         if (c >= '0' && c <= '9') {
+            // 기존 temp에 10 곱하고
             temp *= 10;
+            // ASCii 값을 이용해 수 확인
             temp += c - '0';
         }
+        
+        // 숫자가 아닌 경우
         else if (temp != 0) {
+            // answer에 temp를 더하고 temp 초기화
             answer += temp;
             temp = 0;
         }
     }
     
+    // 숫자로 끝나서 temp가 남은 경우
+    // answer에 더함
     if (temp != 0) answer += temp;
     
     return answer;
