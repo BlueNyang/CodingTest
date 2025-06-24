61cad90d6ac1e6a75a539249125b10b048cd82a1
BlueNyang <xpsj20@gmail.com>
2025-06-24 12:33:32 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int count = 0;
+    
+    while (order > 0) {
+        int digit = order % 10;
+        if (digit != 0 && digit % 3 == 0) count++;
+        order /= 10;
+    }
+    
+    return count;
+}
