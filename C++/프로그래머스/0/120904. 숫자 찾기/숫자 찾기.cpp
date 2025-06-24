ee4691ee8c75d6a4b234a51bc86f90cf365e0077
BlueNyang <xpsj20@gmail.com>
2025-06-24 17:50:51 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- C++/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
+++ C++/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int solution(int num, int k) {
     int answer = 1;
-    string num_str = to_string(num);
     
-    for(const auto n: num_str) {
-        if(n - '0' == k) break;
-        answer++;
+    int temp = num; // 자릿수 계산용
+    long long weight = 1; // 자릿수 가중치
+    
+    // 자릿수 가중치
+    while(temp / 10 > 0) {
+        weight *= 10;
+        temp /= 10;
     }
     
-    if (answer > num_str.length()) return -1;    
-    return answer;
+    // 순회
+    while(weight > 0) {
+        // 숫자를 찾으면 index값 반환
+        if (num / weight == k) return answer;
+        // 아니면 계속 순회
+        answer++;
+        num %= weight;
+        weight /= 10;
+    }
+    // 못찾고 순회가 끝나면 -1
+    return -1;
 }
