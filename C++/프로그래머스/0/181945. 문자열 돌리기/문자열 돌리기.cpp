69ef92ca288b2334218b8b3d93d0b0124f869921
BlueNyang <xpsj20@gmail.com>
2025-06-26 21:51:23 +0900

    [level 0] Title: 문자열 돌리기, Time: 6.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for (const auto& c: str) {
+        cout << c << "\n";
+    }
+    
+    return 0;
+}
