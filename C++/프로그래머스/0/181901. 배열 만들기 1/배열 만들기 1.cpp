2bcd9d6c6ea1e97592956966b47bd2d5306c0998
BlueNyang <xpsj20@gmail.com>
2025-06-29 02:29:17 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.70 ms, Memory: 4.32 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int k) {
+    vector<int> answer;
+    
+    answer.reserve(n / k);
+    
+    for(int i = k; i <= n; i += k)
+        answer.emplace_back(i);
+        
+    return answer;
+}
