216be486005e2ad7b797ad562f3e3a15e5d2007a
BlueNyang <xpsj20@gmail.com>
2025-07-04 13:21:34 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.00 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ Go/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.go
@@ -0,0 +1,9 @@
+func solution(my_string string) string {
+    runes := []rune(my_string)
+    
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    
+    return string(runes)
+}
