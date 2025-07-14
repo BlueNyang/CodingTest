55427a85ae959c1daa3336b6844940f7d6673655
BlueNyang <xpsj20@gmail.com>
2025-07-14 12:38:50 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- C++/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
+++ C++/프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
@@ -5,14 +5,11 @@
 using namespace std;
 
 int solution(vector<vector<int>> lines) {
-    int min = 100, max = -100;
     map<int, int> line_list;
     
     for(const auto& l: lines) {
         for (int i = l[0]; i < l[1]; i++) {
             line_list[i]++;
-            if (i < min) min = i;
-            if (i > max) max = i;
         }
     }
     
